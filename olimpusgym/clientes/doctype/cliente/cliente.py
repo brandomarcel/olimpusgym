ae59a25a25dafe2c0d4799386b31fc8adf7a177e
ORLANDO <edwin_orlando83@gmail.com>
2023-07-21 06:36:56 -0500

    cambios pumiso

--- olimpusgym/clientes/doctype/cliente/cliente.py
+++ olimpusgym/clientes/doctype/cliente/cliente.py
@@ -3,7 +3,7 @@
 
 import base64
 import frappe
-import qrcode
+#import qrcode
 from datetime import date, datetime
 from frappe.model.document import Document
 import pyqrcode
@@ -258,16 +258,16 @@ def deletePeso(name):
     return retorno
 
 
-@frappe.whitelist(allow_guest=True)
+""" @frappe.whitelist(allow_guest=True)
 def generarQR():
     qr = qrcode.QRCode(version=1, box_size=10, border=5)
-    qr.add_data("CLI-2023-0504424987")
-    url = pyqrcode.create("CLI-2023-0504424987", error='H', version=2)
+    qr.add_data("CLI-2023-1722195755")
+    url = pyqrcode.create("CLI-2023-1722195755", error='H', version=2)
     image_as_str = url.png_as_base64_str(scale=2, background=None, quiet_zone=4)
     qr.make(fit=True)
     img = qr.make_image(fill_color="black", back_color="white")
-    img.save("CLI-2023-0504424987.png")
-    with open("CLI-2023-0504424987.png", "rb") as image_file:
+    img.save("CLI-2023-1722195755.png")
+    with open("CLI-2023-1722195755.png", "rb") as image_file:
         try:
             formato = base64.b64encode(image_file.read())
             encoded_string =("La imagen está en formato Base64")
@@ -276,7 +276,7 @@ def generarQR():
        
     return {'encoded_string':encoded_string,
             'formato':formato,
-            'image_as_str':image_as_str}
+            'image_as_str':image_as_str} """
 
 
 
